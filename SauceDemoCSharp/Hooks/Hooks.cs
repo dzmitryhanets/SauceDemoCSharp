b2bae2422893d5d3e8521ea8928807f19e34f294
Dmitry Ganets <dmitry.ganets@amtote.com>
2021-08-10 12:34:09 +0300

    add Allure reporting

---
+++ SauceDemoCSharp/Hooks/Hooks.cs
@@ -0,0 +1,67 @@
+﻿using Allure.Commons;
+using BoDi;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SauceDemoCSharp.Utils;
+using System;
+using System.IO;
+using TechTalk.SpecFlow;
+
+namespace SauceDemoCSharp.Hooks
+{
+    [Binding]
+    public sealed class SpecFlowHooks
+    {
+        private readonly IObjectContainer container;
+        private readonly ScenarioContext scenarioContext;
+        private AllureLifecycle allureLifecycle;
+
+        public SpecFlowHooks(IObjectContainer Container, ScenarioContext ScenarioContext)
+        {
+            container = Container;
+            scenarioContext = ScenarioContext;
+            allureLifecycle = AllureLifecycle.Instance;
+        }
+
+        private void AllureHackForScenarioOutlineTests()
+        {
+            scenarioContext.TryGetValue(out TestResult testresult);
+            allureLifecycle.UpdateTestCase(testresult.uuid, tc =>
+            {
+                tc.name = scenarioContext.ScenarioInfo.Title;
+                tc.historyId = Guid.NewGuid().ToString();
+            });
+        }
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
+        }
+        
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            var capabilitiesGenerator = new CapabilitiesGenerator();
+            IWebDriver driver = capabilitiesGenerator.Create(BrowserType.FireFox);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            // Make this instance available to all other step definitions
+            container.RegisterInstanceAs(driver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            IWebDriver driver = container.Resolve<IWebDriver>();
+            if (scenarioContext.TestError != null)
+            {
+                var path = WebElementsUtils.MakeScreenshot(driver);
+                allureLifecycle.AddAttachment(path);
+            }
+            driver.Close();
+            driver.Dispose();
+            AllureHackForScenarioOutlineTests();
+        }
+    }
+}
---
+++ SauceDemoCSharp/Utils/WebElementsUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+
+namespace SauceDemoCSharp.Utils
+{
+    public static class WebElementsUtils
+    {
+        public static string MakeScreenshot(IWebDriver driver, string testName = "screen")
+        {
+            string projectPath = Path.GetDirectoryName(GetTestAssemblyFolder());
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            string fileLocation = $"{projectPath}/{testName}.png";
+            ss.SaveAsFile(fileLocation, ScreenshotImageFormat.Png);
+            return fileLocation;
+        }
+
+        private static string GetTestAssemblyFolder()
+        {
+            return Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
